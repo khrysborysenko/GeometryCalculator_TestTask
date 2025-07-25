ecfdaa5089f2d76e183b48175ce4214284619554
khrysborysenko <khrystynaaborysenko@gmail.com>
2025-07-25 19:10:17 +0300

    Add standard input

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 
 
-print("Hello World")
+def standard_input():
+    print("Enter the shapes")
+    lines = []
+    while True:
+        try:
+            line = input()
+            if not line.strip():
+                break
+            lines.append(line)
+        except EOFError:
+            break
+    return lines
+
+
+def main():
+    lines = []
+    print("Choose input type: \n1. Keyboard \n2. File")
+    choice = input()
+
+    if choice == "1":
+        lines = standard_input()
+    elif choice == "2":
+        print("OK")
+    else:
+        print("Wrong input")
+        return
+
+    for line in lines:   #temporary check
+        print(f"- {line}")
+
+if __name__ == "__main__":
+    main()
